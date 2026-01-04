d71e568a19d9bc09c9a0cf157d3ef48f24b56085
Ambika Deshmukh <119692705+ambi-04@users.noreply.github.com>
2026-01-04 21:36:42 +0530

    Time: 1 ms (99.76%), Space: 44.4 MB (68.18%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,31 +1,49 @@
 class Solution 
 {
-    public static boolean func(String s, int l, int r)
+    public static boolean isLetterOrDigit(char ch)
     {
-        if(l > r)
+        if ((ch >= 'A' && ch <= 'Z')||(ch >= 'a' && ch <= 'z')||(ch >= '0' && ch <= '9'))
             return true;
-        char ch1 = s.charAt(l);
-        char ch2 = s.charAt(r);
-        if(ch1  >= 65 && ch1 <= 90)
-        {
-            ch1 = (char)(ch1  + 32);
-        }
-        if(ch2 >= 65 && ch2 <= 90)
-        {
-            ch2 = (char)(ch2  + 32);
-        }
-        if(!(ch1 >= 48 && ch1 <= 57) && !(ch1 >= 97 && ch1 <= 122) )
-            return func(s,l+1,r);
-        if(!(ch2 >= 48 && ch2 <= 57) && !(ch2 >= 97 && ch2 <= 122) )
-            return func(s,l,r - 1);
-        if(ch1 == ch2)
-            return func(s,l + 1,r - 1);
-        return false;
-        
+        else
+            return false;
+
     }
     public boolean isPalindrome(String s) 
     {
-        return func(s,0,s.length() - 1);
+        //without creating a separate string
+        boolean result = true;
+        int i = 0;
+        int j = s.length() - 1;
+        while(i <= j)
+        {
+            char ch1 = s.charAt(i);
+            char ch2 = s.charAt(j);
+            if(!isLetterOrDigit(ch1))
+            {
+                i++;
+                continue;
+            }
+            if(!isLetterOrDigit(ch2))
+            {
+                j--;
+                continue;
+            }
+            if(ch1 >= 'A' && ch1 <= 'Z')
+                ch1 = (char)(ch1 + 32);
+            if(ch2 >= 'A' && ch2 <= 'Z')
+                ch2 = (char)(ch2 + 32);
+            //System.out.println(ch1 + " " + ch2);
+            if(ch1 != ch2)
+            {
+                result = false;
+                break;
+            }
+            i++;
+            j--;
+            
+        }
+        return result;
+
 
     }
 }
