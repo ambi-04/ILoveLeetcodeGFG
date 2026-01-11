04a7496b33554748e9a96c26f0b1f15b4283ea0c
Ambika Deshmukh <119692705+ambi-04@users.noreply.github.com>
2026-01-11 23:41:44 +0530

    added bit masking approach

---
+++ GFG Set kth bit/Solution2.java
@@ -0,0 +1,9 @@
+// User function Template for Java
+//Bit masking approach
+class Solution2 {
+    static int setKthBit(int n, int k) {
+        // code here
+        
+        return n | (1 << k);
+    }
+}
