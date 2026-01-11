425852c59fd711bb3a2c4218e1010f3b84ee81f7
Ambika Deshmukh <119692705+ambi-04@users.noreply.github.com>
2026-01-11 16:49:59 +0530

    Add Solution: Check K-th Bit(GFG)
    https://www.geeksforgeeks.org/problems/check-whether-k-th-bit-is-set-or-not-1587115620/1

---
+++ GFG Check K-th Bit/Solution.java
@@ -0,0 +1,10 @@
+class CheckBit {
+    static boolean checkKthBit(int n, int k) {
+        // code here
+        n = n >> k;
+        if(n % 2 != 0)
+            return true;
+        else
+            return false;
+    }
+}
