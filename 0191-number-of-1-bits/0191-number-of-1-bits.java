20b416a0123cf9daf725d8280ddbd00305239e09
Ambika Deshmukh <119692705+ambi-04@users.noreply.github.com>
2026-01-14 23:09:52 +0530

    Time: 0 ms (100%), Space: 42.5 MB (50.28%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -4,9 +4,8 @@ class Solution {
         int cnt = 0;
         while(n != 0)
         {
-            if((n & 1) != 0)
-                cnt++;
-            n = n >> 1;
+            n = n & (n - 1);
+            cnt++;
         }
         return cnt;
         
