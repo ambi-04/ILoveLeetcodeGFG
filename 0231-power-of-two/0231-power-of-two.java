3e5f3fc0bce216d22f5dc371a1047adce5b9eb46
Ambika Deshmukh <119692705+ambi-04@users.noreply.github.com>
2026-01-15 15:23:18 +0530

    Time:1 ms (95.47%) , Space:42.9 MB (9.64%) 42.9 MB (9.64%), 01-15-2026,  Easy- LeetHub

--- 0231-power-of-two/0231-power-of-two.java
+++ 0231-power-of-two/0231-power-of-two.java
@@ -1,7 +1,15 @@
 class Solution {
     public boolean isPowerOfTwo(int n) 
     {
-        return n > 0 && (n & (n - 1)) == 0;
+        if(n < 0)
+            return false;
+        for(int i = 0; i <= 30; i++)
+        {
+            int num = (int)Math.pow(2,i);
+            if(n == num)
+                return true;
+        }
+        return false;
         
     }
 }
