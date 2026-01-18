07487b1e12669cad984514710d68876b0413c7a5
Ambika Deshmukh <119692705+ambi-04@users.noreply.github.com>
2026-01-18 15:50:02 +0530

    Time:1 ms (99.73%) , Space:43.8 MB (22.42%),      01-18-2026,  Easy - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,30 +1,55 @@
-class Solution {
+class Solution 
+{
+    int addTwoBits(int a, int b)
+    {
+        if(a == 1 && b == 1)
+            return 0;
+        else if (a == 0 && b == 0)
+            return 0;
+        else
+            return 1;
+
+    }
+    int carryGenerated(int a, int b)
+    {
+        if(a == 1 && b == 1)
+            return 1;
+        else
+            return 0;
+    }
     public String addBinary(String a, String b) 
     {
         if(a.length() < b.length())
             return addBinary(b,a);
-        int sum = 0;
         int carry = 0;
-        int na = a.length();
-        int nb = b.length();
-        int i = na - 1;
-        int j = nb - 1;
         StringBuilder result = new StringBuilder();
+        int i = a.length() - 1;
+        int j = b.length() - 1;
         while(i >= 0)
         {
-            sum = carry;
-            sum = sum + a.charAt(i) - '0';
+            //System.out.println("carry" + carry);
+            int bita = a.charAt(i) - '0';
+            int bitb = 0;
             if(j >= 0)
-                sum = sum + b.charAt(j) - '0';
-            carry = sum / 2;
-            sum = sum % 2;
-            result.append(sum);
+                bitb = b.charAt(j) - '0';
+            int sum = 0;
+            int new_carry = carryGenerated(bita,bitb);
+            sum = addTwoBits(bita,bitb);
+            new_carry = new_carry + carryGenerated(sum,carry);
+            sum = addTwoBits(sum,carry);
+            
+            //System.out.println("new_carry" + new_carry);
+            //System.out.println("sum" + sum);
+            carry = new_carry;
+            result.append((char)(sum + '0'));
             i--;
             j--;
+
         }
         if(carry != 0)
-            result.append(carry);
+            result.append((char)(carry + '0'));
         return result.reverse().toString();
+
 
         
     }
