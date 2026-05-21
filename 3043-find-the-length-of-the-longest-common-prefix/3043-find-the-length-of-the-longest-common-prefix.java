3413788927eb458b643026fb05c0584685832a68
Ambika Deshmukh <119692705+ambi-04@users.noreply.github.com>
2026-05-22 02:20:26 +0530

    Time: 65 ms (58.97%), Space: 76.8 MB (95.44%) - LeetHub

---
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int longestCommonPrefix(int[] arr1, int[] arr2) 
+    {
+        HashSet<Integer> set = new HashSet<>();
+        for(int x : arr1)
+        {
+            while(x != 0)
+            {
+                set.add(x);
+                x = x/10;
+            }
+
+        }
+        int max = 0;
+        for(int y : arr2)
+        {
+            while(y != 0)
+            {
+                if(set.contains(y) && y > max)
+                    max = y;
+                y = y/10;
+            }
+        }
+        int cnt = 0;
+        while(max != 0)
+        {
+            cnt++;
+            max = max/10;
+        }
+        return cnt;
+    }
+}
