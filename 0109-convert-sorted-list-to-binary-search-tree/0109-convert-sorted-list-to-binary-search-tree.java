8b09c9a66b773a66593a1a2e6bad628e7414b32c
Ambika Deshmukh <119692705+ambi-04@users.noreply.github.com>
2026-06-11 01:58:31 +0530

    Time: 1 ms (17.22%), Space: 47 MB (47.09%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.java
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.java
@@ -25,12 +25,12 @@
  */
 class Solution 
 {
-    public static TreeNode solution(ArrayList<TreeNode> list, int l, int r)
+    public static TreeNode solution(ArrayList<Integer> list, int l, int r)
     {
         if(l <= r)
         {
             int mid = l + (r - l)/2;
-            TreeNode root = list.get(mid);
+            TreeNode root = new TreeNode(list.get(mid));
             root.left = solution(list,l,mid - 1);
             root.right = solution(list,mid + 1,r);
             return root;
@@ -40,11 +40,11 @@ class Solution
 
     public TreeNode sortedListToBST(ListNode head) 
     {
-        ArrayList<TreeNode> list = new ArrayList<>();
+        ArrayList<Integer> list = new ArrayList<>();
         ListNode curr = head;
         while(curr != null)
         {
-            list.add(new TreeNode(curr.val));
+            list.add(curr.val);
             curr = curr.next;
         }
         return solution(list,0,list.size() - 1);
