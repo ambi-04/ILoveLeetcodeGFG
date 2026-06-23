b9045be74d2987b5472ff9a77511158e0b3bd018
Ambika Deshmukh <119692705+ambi-04@users.noreply.github.com>
2026-06-24 03:22:21 +0530

    Time: 0 ms (100%), Space: 43.9 MB (16.24%) - LeetHub

--- 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.java
+++ 3432-count-partitions-with-even-sum-difference/3432-count-partitions-with-even-sum-difference.java
@@ -6,16 +6,7 @@ class Solution {
             sum += x;
         if(sum % 2 != 0)
             return 0;
-        int partition_sum = 0;
-        int cnt = 0;
-        for(int i = 0; i < nums.length - 1; i++)
-        {
-            partition_sum += nums[i];
-            int diff = Math.abs(partition_sum - (sum - partition_sum));
-            if(diff % 2 == 0)
-                cnt++;
-        }
-        return cnt;
+        return nums.length - 1;
         
     }
 }
