2db8281a66e436105083a5b7c1f83a80e4b82fa6
Ambika Deshmukh <119692705+ambi-04@users.noreply.github.com>
2026-07-05 16:03:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Cut rope to maximise product/cut-rope-to-maximise-product.java
+++ Difficulty: Medium/Cut rope to maximise product/cut-rope-to-maximise-product.java
@@ -1,25 +1,22 @@
-class Solution 
-{
-    public int func(int n, int[] dp){
-        if (n == 1)
-            return 1;
-        if(dp[n] != 0)
-            return dp[n];
-        int max = 0;
-        for(int i = 1; i < n ; i++)
-        {
-            int product = Math.max(i * (n - i), func(i,dp) * func(n - i,dp));
-            product = Math.max(func(i,dp) * (n - i), product);
-            product = Math.max(func(n - i,dp) * i, product);
-            max = Math.max(max, product);
-        }
-        dp[n] = max;
-        return max;
-        
-    }
-    public int maxProduct(int n ) {
+class Solution {
+    public int maxProduct(int n) 
+    {
         // code here
-        return func(n,new int[n + 1]);
+        if(n == 2 || n == 3)
+            return n - 1;
+        if(n == 4)
+            return 4;
+        int num_pieces3 = n/3;
+        int last_piece = n % 3;
+        if(last_piece == 1)
+        {
+            last_piece = 4;
+            num_pieces3 --;
+        }
+        int result = (int)Math.pow(3,num_pieces3);
+        if(last_piece != 0)
+            result = result * last_piece;
+        return result;
         
     }
 }
